d1bba6a06186b43d0681437898184739167cfdc0
oncomouse <oncomouse@gmail.com>
2018-08-14 12:24:03 -0500

    Working CLI Script

---
+++ cli.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+const postcss = require('postcss');
+const cssnano = require('cssnano');
+const argv = require('minimist')(process.argv.slice(2), {
+  boolean: [
+    'compress',
+  ],
+  string: [
+    'output',
+    'o',
+    'out',
+  ],
+  alias: {
+    'output': [
+      'o',
+      'out',
+    ],
+  },
+  default: {
+    'output': null,
+  },
+});
+const { extractClasses, extractTachyons } = require('./index');
+
+// Calculate the union between two lists:
+function union(a, b) {
+  return a.concat(b.filter(function (item) {
+    return a.indexOf(item) < 0;
+  }));
+}
+
+
+// Build a complete list of all CSS classes used in the supplied documents:
+const htmlClasses = argv._.reduce((acc, cur) => union(acc, extractClasses(fs.readFileSync(cur).toString())), []);
+const customCSS = extractTachyons(htmlClasses);
+
+function handleOutput(css) {
+  if(argv.output === null) {
+    console.log(css);
+  } else {
+    fs.writeFileSync(argv.output, css);
+  }
+}
+
+// If we are compressing, run CSSNano, otherwise print:
+if(argv.compress) {
+  postcss([cssnano({preset: 'default'})]).process(customCSS, { from: undefined }).then(result => handleOutput(result.css));
+} else {
+  handleOutput(customCSS);
+}
--- index.js
+++ index.js
@@ -1,27 +1,6 @@
-const fs = require('fs');
-const path = require('path');
-const argv = require('minimist')(process.argv.slice(2), {
-  boolean: [
-    'compress',
-  ],
-  string: [
-    'output',
-    'o',
-    'out',
-  ],
-  alias: {
-    'output': [
-      'o',
-      'out',
-    ],
-  },
-  default: {
-    'output': null,
-  },
-});
 const { JSDOM } = require('jsdom');
 const postcss = require('postcss');
-const cssnano = require('cssnano');
+const fs = require('fs');
 // Read CSS files as text:
 require.extensions['.css'] = function (module, filename) {
     module.exports = fs.readFileSync(filename, 'utf8');
@@ -31,19 +10,10 @@ const tachyonsImageCSS = require('tachyons-images/css/tachyons-images.min.css');
 const tachyonsBoxSizingCSS = require('tachyons-box-sizing/css/tachyons-box-sizing.min.css');
 const normalizeCSS = require('normalize.css/normalize.css');
 
-// Calculate the union between two lists:
-function union(a, b) {
-  return a.concat(b.filter(function (item) {
-    return a.indexOf(item) < 0;
-  }));
-}
-
 // Extract all CSS classes from an HTML file.
 // This can probably be done without JSDOM, but it seems more reliable than
 // to use Regex or something like that:
-function extractClasses(htmlFilePath) {
-  const html = fs.readFileSync(htmlFilePath).toString();
-
+function extractClasses(html) {
   const document = (new JSDOM(html)).window.document;
 
   const htmlClasses = [];
@@ -58,40 +28,38 @@ function extractClasses(htmlFilePath) {
   return htmlClasses;
 }
 
-// Build a complete list of all CSS classes used in the supplied documents:
-const htmlClasses = argv._.reduce((acc, cur) => union(acc, extractClasses(cur)), []);
+function extractTachyons(htmlClasses) {
+  // Use PostCSS's AST to build the list of Tachyons used in the html files:
+  const tachyonsAST = postcss.parse(tachyonsCSS);
+  const rootCSS = [];
+  const mediaQueries = {};
+  // Extract a list of all classes in Tachyons:
+  // const tachyonsClasses = [];
+  // tachyonsAST.walkRules(/^\./, (rule) => {
+  //   const outRule = rule.selector.replace(/\s+$/,'').replace(/^\./,'');
+  //   if(tachyonsClasses.indexOf(outRule) === -1) {
+  //     tachyonsClasses.push(outRule);
+  //   }
+  // });
 
-// Use PostCSS's AST to build the list of Tachyons used in the html files:
-const tachyonsAST = postcss.parse(tachyonsCSS);
-const rootCSS = [];
-const mediaQueries = {};
-// Extract a list of all classes in Tachyons:
-// const tachyonsClasses = [];
-// tachyonsAST.walkRules(/^\./, (rule) => {
-//   const outRule = rule.selector.replace(/\s+$/,'').replace(/^\./,'');
-//   if(tachyonsClasses.indexOf(outRule) === -1) {
-//     tachyonsClasses.push(outRule);
-//   }
-// });
-
-// Extract a list of media queries:
-tachyonsAST.walkAtRules('media', (rule) => {
-  mediaQueries[rule.params] = [];
-});
-// Walk our classes and, if they are defined in Tachyons, add them to our custom
-// file:
-htmlClasses.forEach((cssClass) => {
-  tachyonsAST.walkRules(`.${cssClass}`, (rule) => {
-    if(rule.parent.type === 'root') {
-      rootCSS.push(rule.toString());
-    } else {
-      mediaQueries[rule.parent.params].push(rule.toString());
-    }
+  // Extract a list of media queries:
+  tachyonsAST.walkAtRules('media', (rule) => {
+    mediaQueries[rule.params] = [];
   });
-});
+  // Walk our classes and, if they are defined in Tachyons, add them to our custom
+  // file:
+  htmlClasses.forEach((cssClass) => {
+    tachyonsAST.walkRules(`.${cssClass}`, (rule) => {
+      if(rule.parent.type === 'root') {
+        rootCSS.push(rule.toString());
+      } else {
+        mediaQueries[rule.parent.params].push(rule.toString());
+      }
+    });
+  });
 
-// Define our output and include some reset code:
-const customCSS = `${normalizeCSS}
+  // Define our output and include some reset code:
+  const customCSS = `${normalizeCSS}
 ${tachyonsBoxSizingCSS}
 ${tachyonsImageCSS}
 ${rootCSS.join('\n')}
@@ -99,17 +67,11 @@ ${Object.keys(mediaQueries).filter(x => mediaQueries[x].length !== 0).map(key =>
 ${mediaQueries[key].join('\n')}
 }`).join('\n')}`;
 
-function handleOutput(css) {
-  if(argv.output === null) {
-    console.log(css);
-  } else {
-    fs.writeFileSync(argv.output, css);
-  }
+  return customCSS;
+
 }
 
-// If we are compressing, run CSSNano, otherwise print:
-if(argv.compress) {
-  postcss([cssnano({preset: 'default'})]).process(customCSS, { from: undefined }).then(result => handleOutput(result.css));
-} else {
-  handleOutput(customCSS);
-}
+module.exports = {
+  extractTachyons,
+  extractClasses
+};
